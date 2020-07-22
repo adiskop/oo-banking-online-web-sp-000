9361ca345d1882c8151471c4e64c05d87cfece9e
Adi Skop <adiskop@gmail.com>
2020-07-22 14:14:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,13 +1,10 @@
 class Transfer
-  attr_reader  :sender, :receiver, :amount
+  attr_reader :amount, :sender, :receiver
   attr_accessor :status
 
-def initialize(sender, receiver, amount)
-@status="pending"
-@sender=sender
-@reciever=reciever
-@amount=amount
-end
-
-
-end
+  def initialize(sender, receiver, amount)
+    @status = "pending"
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+  end
