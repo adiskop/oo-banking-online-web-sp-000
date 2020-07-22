f0820ff3fe0c22f6903adb432d6ab87bb31cab65
Adi Skop <adiskop@gmail.com>
2020-07-22 14:35:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,12 +25,23 @@ def execute_transaction
     end
   end
 
+
+  def reverse_transfer
+      if valid? && receiver.balance > amount && self.status == "complete"
+        receiver.balance -= amount
+        sender.balance += amount
+        self.status = "reversed"
+      else
+        reject_transfer
+      end
+    end
+
 def reject_transfer
    self.status = "rejected"
    "Transaction rejected. Please check your account balance."
  end
 
-end
+
 
 
 end
