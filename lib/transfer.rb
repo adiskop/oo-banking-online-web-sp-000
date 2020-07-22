beca424e0c6ffecc9d198c7a72746c7057631a47
Adi Skop <adiskop@gmail.com>
2020-07-22 14:11:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
-  attr_accessor :sender, :receiver, :status
-
+  attr_accessor :status
+  attr_reader  :sender, :receiver
 def initialize(sender, receiver, amount)
 @sender=sender
 @reciever=reciever
