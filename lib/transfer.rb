aedd8627635fbab792056e191664e251a4c1897d
Adi Skop <adiskop@gmail.com>
2020-07-22 14:34:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ sender.balance= sender.balance-amount
 receiver.balance=receiver.balance+amount
 self.status="complete"
 else
-  status = "rejected"
+ "Transaction rejected. Please check your account balance."
 end
 end
 
