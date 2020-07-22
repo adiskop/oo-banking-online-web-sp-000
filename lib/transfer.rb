a0da69db111aed734a4487fb2af174c4b0d87aec
Adi Skop <adiskop@gmail.com>
2020-07-22 14:35:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,13 +16,14 @@ def valid?
 end
 
 def execute_transaction
-  if valid? && sender.balance > amount && self.status == "pending"
-sender.balance= sender.balance-amount
-receiver.balance=receiver.balance+amount
-self.status="complete"
-else
-    reject_transfer
-end
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      reject_transfer
+    end
+  end
 
 def reject_transfer
    self.status = "rejected"
